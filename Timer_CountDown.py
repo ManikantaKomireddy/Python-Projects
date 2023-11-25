f81258f3cc4f83bd91ef9d8748605c9488bed795
Manikanta Komireddy <manikomireddy@gmail.com>
2023-11-25 20:56:05 +0530

    Timer Count

---
+++ Timer_CountDown.py
@@ -0,0 +1,16 @@
+import time
+
+def countDownTimer(t):
+    while t:
+        min,sec=divmod(t,60)
+        timer='{:02d} : {:02d}'.format(min,sec)
+        print(timer)
+        time.sleep(1)
+        t=t-1
+    
+    print("Timer completed sucessfully")
+    
+t=int(input("Enter Time in seconds"))
+countDownTimer(t)
+        
+    
