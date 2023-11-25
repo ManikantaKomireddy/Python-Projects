56f6c79f18a78f69ca4f3dbc8682a52726e99834
Manikanta Komireddy <manikomireddy@gmail.com>
2023-11-25 21:02:12 +0530

    Timer Count seond commit

--- Timer_CountDown.py
+++ Timer_CountDown.py
@@ -4,7 +4,7 @@ def countDownTimer(t):
     while t:
         min,sec=divmod(t,60)
         timer='{:02d} : {:02d}'.format(min,sec)
-        print(timer)
+        print(timer, end="\r")
         time.sleep(1)
         t=t-1
     
